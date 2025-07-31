3c074a906280c9971132376da487157c14e50218
MuhammadShameel <mshameelkz@gmail.com>
2025-07-31 09:16:46 +0500

    Refactor Header component to use motion.header for improved semantics; reorganize layout structure for better readability

--- app/components/layout/Header.jsx
+++ app/components/layout/Header.jsx
@@ -18,38 +18,41 @@ const navLinks = [
 
 const Header = () => {
   return (
-    <motion.nav
+    <motion.header
       // Framer Motion animation properties
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.8, ease: "easeInOut" }}
-      className="container mx-auto pl-[30px] pr-2.5 py-[10px] fixed top-4 left-1/2 -translate-x-1/2 w-full rounded-[10px] z-50
-                 [background:linear-gradient(0deg,rgba(255,255,255,0.2)_0%,rgba(255,255,255,0.2)_100%),rgba(0,0,0,0.05)] backdrop-blur-[15px]"
+      className="fixed top-4 left-1/2 -translate-x-1/2 w-full z-50 lg:px-5 md:px-4 px-3"
     >
-      <div className="flex items-center justify-between">
-        {/* Logo */}
-        <div className="flex flex-col items-center">
-          <Image src={Logo} alt=""></Image>
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between pl-[30px] pr-2.5 py-[10px] rounded-[10px] [background:linear-gradient(0deg,rgba(255,255,255,0.2)_0%,rgba(255,255,255,0.2)_100%),rgba(0,0,0,0.05)] backdrop-blur-[15px]">
+          {/* Logo */}
+          <div className="flex flex-col items-center">
+            <Image src={Logo} alt=""></Image>
+          </div>
+
+          {/* Navigation Links */}
+          <nav>
+            <ul className="hidden md:flex items-center gap-10">
+              {navLinks.map((link) => (
+                <li key={link.name}>
+                  <a
+                    href={link.href}
+                    className="color-dark-cards-bg font-medium hover:color-dark transition-colors text-xl"
+                  >
+                    {link.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          {/* Contact Button */}
+          <button className="btn btn-primary">Contact Us</button>
         </div>
-
-        {/* Navigation Links */}
-        <ul className="hidden md:flex items-center gap-10">
-          {navLinks.map((link) => (
-            <li key={link.name}>
-              <a
-                href={link.href}
-                className="color-dark-cards-bg font-medium hover:color-dark transition-colors text-xl"
-              >
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-
-        {/* Contact Button */}
-        <button className="btn btn-primary">Contact Us</button>
       </div>
-    </motion.nav>
+    </motion.header>
   );
 };
 
