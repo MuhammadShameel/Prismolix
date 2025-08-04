74af892fbad900a2616fad3f484b6fdfb1db4508
MuhammadShameel <mshameelkz@gmail.com>
2025-08-04 10:36:09 +0500

    Enhance typography for small screens in globals.css; adjust font sizes for headings and paragraphs, and add border-radius to buttons

--- app/page.js
+++ app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <ProblemSolution />
-      <Services />
+      <ProblemSolution />
+      {/* <Services />
       <Testimonial />
       <ContactSection /> */}
     </>
