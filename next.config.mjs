3a02253252afd917ff1199043f5ab18febb67a1e
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2025-08-06 11:06:20 +0500

    Implement dynamic blog sections with API integration; add image URL handling and improve layout for portfolio items.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'prismolix.wasmer.app',
+        port: '',
+        pathname: '/wp-content/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prismolix.wasmer.app',
+        port: '',
+        pathname: '/wp-content/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
