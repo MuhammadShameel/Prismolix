aab520568f6f99cc7bed8421e51e4b0aac0f6f8e
MuhammadShameel <mshameelkz@gmail.com>
2025-09-03 13:30:26 +0500

    make button work

--- app/components/Testimonial.jsx
+++ app/components/Testimonial.jsx
@@ -124,11 +124,11 @@ const Testimonial = () => {
             </Swiper>
 
             {/* Custom Navigation Buttons */}
-            <div className="mt-8 flex items-end justify-end gap-4 md:absolute md:bottom-8 md:right-8 md:mt-0">
+            <div className="mt-8 flex items-end justify-end gap-4 md:absolute md:bottom-8 md:right-8 md:mt-0 z-1">
               <button
                 onClick={handlePrev}
                 aria-label="Previous testimonial"
-                className="flex h-12 w-12 p-2.5 items-center justify-center rounded-full bg-light-purple text-gray-800 transition hover:bg-gray-200"
+                className="flex h-12 w-12 p-2.5 items-center justify-center rounded-full bg-light-purple text-gray-800  hover:bg-gray-200  cursor-pointer transition-colors duration-200  ease-in-out  focus:outline-none focus:ring-2 focus:ring-[#f2ebff] focus:ring-offset-2"
               >
                 {/* SVG Icon for Left Arrow */}
                 <svg
@@ -159,7 +159,7 @@ const Testimonial = () => {
               <button
                 onClick={handleNext}
                 aria-label="Next testimonial"
-                className="flex h-12 w-12  p-2.5 items-center justify-center rounded-full bg-violet-600 text-white transition-colors duration-200 ease-in-out hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2"
+                className="flex h-12 w-12  p-2.5 items-center justify-center rounded-full bg-violet-600 text-white transition-colors duration-200  ease-in-out hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2"
               >
                 {/* SVG Icon for Right Arrow */}
                 <svg
