3aa58ef70cbf7a5dad4de6457b24fb38b570db83
Muhammad Shameel <123203015+MuhammadShameel@users.noreply.github.com>
2025-09-17 11:41:53 +0500

    update metadata for SEO optimization in layout and remove redundant metadata from Landing component

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -8,33 +8,6 @@ import ContactSection from "./ContactSection";
 import CallSchedule from "./CallSchedule";
 import FooterMarquee from "../components/FooterMarquee";
 
-
-export const metadata = {
-  title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
-  description:
-    "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
-  alternates: {
-    canonical: "https://www.prismolix.com/",
-  },
-  openGraph: {
-    type: "website",
-    url: "https://prismolix.com",
-    title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
-    description:
-      "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
-    images:
-      "https://prismolix.wasmer.app/wp-content/uploads/2025/08/blogDetail.png",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
-    description:
-      "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
-    images:
-      "https://prismolix.wasmer.app/wp-content/uploads/2025/08/blogDetail.png",
-  },
-};
-
 const Home = () => {
   return (
     <>
