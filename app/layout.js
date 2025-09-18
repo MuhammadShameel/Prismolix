3eee6ee1c0015795ddc02397f2c348966cce3edc
Muhammad Shameel <123203015+MuhammadShameel@users.noreply.github.com>
2025-09-18 08:40:52 +0500

    Refactor code structure for improved readability and maintainability

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
+// "use client";
+
 import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+
+// import { useState, useEffect } from "react";
+// import IntroAnimation from "../app/components/IntroAnimation";
 
 const satoshi = localFont({
   src: [
@@ -62,36 +67,49 @@ const birkenNue = localFont({
   display: "swap",
 });
 
-export const metadata = {
-  title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
-  description:
-    "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
-  alternates: {
-    canonical: "https://www.prismolix.com/",
-  },
-  openGraph: {
-    type: "website",
-    url: "https://prismolix.com",
-    title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
-    description:
-      "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
-    images:
-      "https://prismolix.wasmer.app/wp-content/uploads/2025/08/blogDetail.png",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
-    description:
-      "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
-    images:
-      "https://prismolix.wasmer.app/wp-content/uploads/2025/08/blogDetail.png",
-  },
-};
+// export const metadata = {
+//   title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
+//   description:
+//     "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
+//   alternates: {
+//     canonical: "https://www.prismolix.com/",
+//   },
+//   openGraph: {
+//     type: "website",
+//     url: "https://prismolix.com",
+//     title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
+//     description:
+//       "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
+//     images:
+//       "https://prismolix.wasmer.app/wp-content/uploads/2025/08/blogDetail.png",
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Prismolix • White-Label Design & Dev Agency for Growing Agencies",
+//     description:
+//       "Prismolix helps creative & digital agencies scale faster. We deliver UI/UX, web, app & Shopify design & development. Focus on growth; let us handle execution.",
+//     images:
+//       "https://prismolix.wasmer.app/wp-content/uploads/2025/08/blogDetail.png",
+//   },
+// };
 
 export default function RootLayout({ children }) {
+  // const [showIntro, setShowIntro] = useState(true);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setShowIntro(false), 2200); // match logo animation duration
+  //   return () => clearTimeout(timer);
+  // }, []);
+
   return (
     <html lang="en">
       <body className={`${satoshi.variable} ${birkenNue.variable}`}>
+        {/* {showIntro ? (
+          <IntroAnimation />
+        ) : (
+          <main className="fade-in">{children}</main>
+        )} */}
+
         <Header />
         {children}
         <Footer />
