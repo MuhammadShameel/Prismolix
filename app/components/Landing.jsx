4bdd3aa6f010338537c10a7cf9ad74f2833c6080
Muhammad Shameel <123203015+MuhammadShameel@users.noreply.github.com>
2025-10-08 10:56:10 +0500

    refactor: Remove unused imports in Landing component for cleaner code

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import HeroSection from "./HeroSection";
-import Image from "next/image";
 import ProblemSolution from "./ProblemSolution";
 import Services from "./Services";
 import Testimonial from "./Testimonial";
 import ContactSection from "./ContactSection";
 import CallSchedule from "./CallSchedule";
 import FooterMarquee from "../components/FooterMarquee";
-import AnimationWrapper from "./IntroAnimation";
 
 const schemaData = {
   "@context": "https://schema.org",
